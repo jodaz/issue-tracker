ed9f23b9f3ae5aa96158a1a20222905b399df6da
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-08-14 10:46:08 -0400

    Create validation files

---
+++ validation/fields.js
@@ -0,0 +1,22 @@
+const isEmpty = require('../isEmpty');
+
+const validateFields = data => {
+  const errors = {};
+
+  if (isEmpty(data.issue_title)) {
+    errors.issue_title = 'Missing issue title';
+  }
+  if (isEmpty(data.issue_text)) {
+    errors.issue_text = 'Missing issue description';
+  }
+  if (isEmpty(data.created_by)) {
+    errors.created_by = 'Missing issue author';
+  }
+
+  return {
+    errors: errors,
+    isValid: isEmpty(errors)
+  }
+};
+
+module.exports = validateFields;
