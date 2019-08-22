d7d708d3a98e30ab0e2f77879c42f47040d58d64
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-08-21 23:07:24 -0400

    Sort issues by most recent

--- controllers/issues/getAll.js
+++ controllers/issues/getAll.js
@@ -3,6 +3,7 @@ const Issue   = require('../../models/Issue');
 module.exports = (req, res) => {
   Issue.find({})
     .populate('project', ['project_name'])
+    .sort({created_on: -1})
     .then(issues => res.json(issues))
     .catch(err => res.status(404).json({'noissues': 'no issues found'}));
 };
